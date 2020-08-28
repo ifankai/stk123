c35c62a3abd770c3c1ef111dbfdfbb88d07e769f
ifankai <ifankai@aliyun.com>
2020-08-28 18:25:19 +0800

    1

--- src/com/stk123/spring/SpringTest.java
+++ src/com/stk123/spring/SpringTest.java
@@ -1,5 +1,6 @@
 package com.stk123.spring;
 
+import com.stk123.spring.jpa.entity.Admapp;
 import com.stk123.spring.jpa.repository.AdmappRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -11,5 +12,9 @@ public class SpringTest {
         AdmappRepository admappRepository = SpringUtils.getBean(AdmappRepository.class);
         long count = admappRepository.count();
         System.out.println(count);
+        Admapp admapp = admappRepository.findByAdapplicationEquals("DB");
+        System.out.println(admapp.getAdalink());
+        admapp.setAdausrcre("SYStem");
+        admappRepository.save(admapp);
     }
 }
--- src/com/stk123/spring/jpa/repository/AdmappRepository.java
+++ src/com/stk123/spring/jpa/repository/AdmappRepository.java
@@ -6,4 +6,7 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AdmappRepository extends JpaRepository<Admapp, String> {
+
+    Admapp findByAdapplicationEquals(String adapplication);
+
 }
