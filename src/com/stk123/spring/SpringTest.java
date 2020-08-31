01899d7ced8802a053e52c44258f623c1c64873d
ifankai <ifankai@aliyun.com>
2020-08-31 18:41:40 +0800

    1

--- src/com/stk123/spring/SpringTest.java
+++ src/com/stk123/spring/SpringTest.java
@@ -4,12 +4,28 @@ import com.stk123.spring.jpa.entity.Admapp;
 import com.stk123.spring.jpa.repository.AdmappRepository;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import java.util.Date;
+
+import static org.hibernate.jpa.AvailableSettings.PERSISTENCE_UNIT_NAME;
 
 @Component
 public class SpringTest {
 
     private static final Log logger = LogFactory.getLog(SpringTest.class);
+
+    private static EntityManagerFactory entityManagerFactory;// = SpringUtils.getBean(EntityManagerFactory.class);
+
+    @Autowired
+    public void setEntityManagerFactory(EntityManagerFactory entityManagerFactory){
+        SpringTest.entityManagerFactory = entityManagerFactory;
+    }
 
     public static void main(String[] args) {
         AdmappRepository admappRepository = SpringUtils.getBean(AdmappRepository.class);
@@ -19,5 +35,9 @@ public class SpringTest {
         logger.info(admapp.getAdalink()+","+admapp.getRowid());
         admapp.setAdausrcre("SYSTEM");
         admappRepository.save(admapp);
+
+        admapp.setAdaupd(new Date());
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        entityManager.merge(admapp);
     }
 }
