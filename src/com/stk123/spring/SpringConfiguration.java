ba50c9829834ed0bab8ed89d6cd8f68485bf0abe
ifankai <ifankai@aliyun.com>
2020-09-01 12:06:43 +0800

    1

--- src/com/stk123/spring/SpringConfiguration.java
+++ src/com/stk123/spring/SpringConfiguration.java
@@ -4,9 +4,8 @@ import com.alibaba.druid.pool.DruidDataSourceFactory;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.hibernate.jpa.HibernatePersistenceProvider;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.*;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
@@ -14,7 +13,10 @@ import org.springframework.orm.jpa.persistenceunit.DefaultPersistenceUnitManager
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 import java.util.HashMap;
 import java.util.Map;
@@ -22,7 +24,8 @@ import java.util.Map;
 @Configuration
 @ComponentScan(basePackages = "com.stk123.spring")
 @EnableJpaRepositories
-//@PropertySource("/META-INF/application.properties")
+@EnableTransactionManagement//(mode = AdviceMode.ASPECTJ)
+@PropertySource("stk.properties")
 public class SpringConfiguration {
 
     private static final Log logger = LogFactory.getLog(SpringConfiguration.class);
@@ -30,6 +33,9 @@ public class SpringConfiguration {
     public SpringConfiguration() {
         logger.info("SpringConfiguration容器启动初始化。。。");
     }
+
+    @Autowired
+    private EntityManagerFactory entityManagerFactory;
 
     @Bean
     public ThreadPoolTaskExecutor setTaskExecutor(){
@@ -40,9 +46,10 @@ public class SpringConfiguration {
         return taskExecutor;
     }
 
-    @Bean(name = "entityManagerFactory")
-    public LocalContainerEntityManagerFactoryBean entityManagerFactory() throws Exception {
+    @Bean//(name = "entityManagerFactory")
+    public EntityManagerFactory entityManagerFactory() {
         HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+        vendorAdapter.setShowSql(true);
         //vendorAdapter.setGenerateDdl(true);
         LocalContainerEntityManagerFactoryBean factory = new LocalContainerEntityManagerFactoryBean();
         factory.setJpaVendorAdapter(vendorAdapter);
@@ -55,26 +62,39 @@ public class SpringConfiguration {
         //HibernatePersistenceProvider hibernatePersistenceProvider = new HibernatePersistenceProvider();
         //factory.setPersistenceProvider(hibernatePersistenceProvider);
         factory.setDataSource(dataSource());
+        factory.afterPropertiesSet();
 
-        return factory;
+        return factory.getObject();
     }
 
     @Bean
-    public DataSource dataSource() throws Exception {
+    public DataSource dataSource() {
         Map properties = new HashMap();
         properties.put("driverClassName", "oracle.jdbc.driver.OracleDriver");
         properties.put("url", "jdbc:oracle:thin:@9.197.4.250:1521:TWP4T1");
         properties.put("username", "p4pst2");
         properties.put("password", "carrefour");
         properties.put("testWhileIdle", "false");
-        return DruidDataSourceFactory.createDataSource(properties);
+
+        DataSource dataSource = null;
+        try {
+            dataSource = DruidDataSourceFactory.createDataSource(properties);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return dataSource;
     }
 
-    @Bean(name = "transactionManager")
-    public PlatformTransactionManager transactionManager() throws Exception {
+    @Bean
+    public PlatformTransactionManager transactionManager() {
         JpaTransactionManager txManager = new JpaTransactionManager();
-        txManager.setEntityManagerFactory(entityManagerFactory().getObject());
+        txManager.setEntityManagerFactory(entityManagerFactory());
         return txManager;
+    }
+
+    @Bean
+    public EntityManager entityManager(){
+        return entityManagerFactory().createEntityManager();
     }
 
 }
