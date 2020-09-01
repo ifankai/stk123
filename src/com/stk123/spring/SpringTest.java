84b88fdadb6783378c4ee6f98567aab6bb60f202
ifankai <ifankai@aliyun.com>
2020-09-01 12:06:43 +0800

    1

--- src/com/stk123/spring/SpringTest.java
+++ src/com/stk123/spring/SpringTest.java
@@ -6,10 +6,13 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
+import javax.annotation.Resource;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import javax.persistence.PersistenceContext;
 
 import java.util.Date;
 
@@ -18,26 +21,40 @@ import static org.hibernate.jpa.AvailableSettings.PERSISTENCE_UNIT_NAME;
 @Component
 public class SpringTest {
 
-    private static final Log logger = LogFactory.getLog(SpringTest.class);
+    private static final Log log = LogFactory.getLog(SpringTest.class);
 
     private static EntityManagerFactory entityManagerFactory;// = SpringUtils.getBean(EntityManagerFactory.class);
 
     @Autowired
     public void setEntityManagerFactory(EntityManagerFactory entityManagerFactory){
+        log.info("setEntityManagerFactory");
         SpringTest.entityManagerFactory = entityManagerFactory;
     }
+
+    @PersistenceContext
+    private EntityManager entityManager;
 
     public static void main(String[] args) {
         AdmappRepository admappRepository = SpringUtils.getBean(AdmappRepository.class);
         long count = admappRepository.count();
-        logger.info(String.valueOf(count));
+        log.info(String.valueOf(count));
         Admapp admapp = admappRepository.findByAdapplicationEquals("DB");
-        logger.info(admapp.getAdalink()+","+admapp.getRowid());
+        log.info(admapp.getAdalink()+","+admapp.getRowid());
         admapp.setAdausrcre("SYSTEM");
         admappRepository.save(admapp);
 
+        SpringTest test = SpringUtils.getBean(SpringTest.class);
         admapp.setAdaupd(new Date());
-        EntityManager entityManager = entityManagerFactory.createEntityManager();
-        entityManager.merge(admapp);
+        test.update(admapp);
     }
+
+    @Transactional
+    public void update(Admapp admapp){
+        //EntityManager em = entityManagerFactory.createEntityManager();
+        //em.getTransaction().begin();
+        entityManager.merge(admapp);
+        entityManager.flush();
+        //em.getTransaction().commit();
+    }
+
 }
