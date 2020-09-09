e265a1a0e012703580ef9091e33e4a411050a084
ifankai <ifankai@aliyun.com>
2020-09-09 16:47:14 +0800

    1

--- src/com/stk123/model/strategy/StrategyManager.java
+++ src/com/stk123/model/strategy/StrategyManager.java
@@ -35,26 +35,26 @@ import com.stk123.web.action.ScreenerAction;
 import com.stk123.web.form.ScreenerForm;
 
 public class StrategyManager {
-	
+
 	private Connection conn = null;
 	private String date;
-	
+
 	public List<Strategy> strategys = new ArrayList<Strategy>();
-	
+
 	public StrategyManager(Connection conn,String date){
 		this.conn = conn;
 		this.date = date;
 	}
-	
+
 	public void init(List<Index> indexs) throws Exception{
-		
+
 		Strategy.allIndexs = indexs;
 		Strategy.followIndexs = getFollowIndex(conn);
-		
+
 		//List<Index> erIndexs = getEarningForecastIndex(conn);
 		//List<Index> search_9 = getSearchCondition(conn, 9);//锟斤拷锟斤拷源锟斤拷锟斤拷锟斤拷业锟斤拷
 		//List<Index> search_16 = getSearchCondition(conn, 16);//小锟斤拷锟斤拷
-		
+
 		strategys.add(new Strategy1());//模型1-60日均线上升且活跃放量
 		strategys.add(new Strategy2());//模型2-120日均线上升60日线下降
 		strategys.add(new Strategy3());//模型3-ENE活跃个股
@@ -72,24 +72,24 @@ public class StrategyManager {
 		strategys.add(new Strategy15());//模型15-巨量后k线振幅缩小且没有调整太深
 		strategys.add(new Strategy16());//模型16-阳线放量阴线缩量
 		//strategys.add(new Strategy17());//K锟竭诧拷锟斤拷
-		
+
 		List<Strategy> slist = getSearchConditionWithStrategy(conn);
 		for(Strategy s : slist){
 			strategys.add(s);
 		}
-		
+
 		List<Index> netProfitGrwothGreaterThanZeroIndexs = getNetProfitGrwothGreaterThanZero(conn);
-		
+
 		//strategys.add(new Strategy20("锟斤拷锟竭短碉拷锟斤拷锟斤拷",getIndexMA20And120(netProfitGrwothGreaterThanZeroIndexs, date),false,true));
 		//strategys.add(new Strategy20("前锟节伙拷锟斤拷锟斤拷200%锟斤拷锟斤拷",getIndexHighHsl(netProfitGrwothGreaterThanZeroIndexs, date),false,true));
-		
+
 		strategys.add(new Strategy19("关注C",Strategy.followIndexs,false,true));//模型19-突破趋势线
 		//strategys.add(new Strategy18("锟斤拷注C",Strategy.followIndexs,false,true));//锟斤拷选锟斤拷k锟竭碉拷位十锟斤拷锟斤拷
 		strategys.add(new Strategy20("关注C",Strategy.followIndexs,false,true));//模型20-十字星
 	}
-	
+
 	public static List<Name2Value<String,Name2Value<Integer, Class>>> APPLY_STRATEGY = null;
-	
+
 	public static List<Name2Value<String,Name2Value<Integer, Class>>> getApplyStrategy() {
 		if(APPLY_STRATEGY != null)return APPLY_STRATEGY;
 		List<Name2Value<String,Name2Value<Integer, Class>>> s = new ArrayList();
@@ -98,7 +98,7 @@ public class StrategyManager {
 		s.add(new Name2Value("模型20-十字星", new Name2Value(20, Strategy20.class)));
 		return APPLY_STRATEGY = s;
 	}
-	
+
 	public static Strategy getStrategyById(String id) throws Exception {
 		List<Name2Value<String,Name2Value<Integer, Class>>> list = getApplyStrategy();
 		for(Name2Value<String,Name2Value<Integer, Class>> s : list){
@@ -108,7 +108,7 @@ public class StrategyManager {
 		}
 		return null;
 	}
-	
+
 	public void execute() throws Exception{
 		for(Strategy strategy : strategys){
 			try{
@@ -119,7 +119,7 @@ public class StrategyManager {
 			}
 		}
 	}
-	
+
 	public static List<Strategy> getSearchConditionWithStrategy(Connection conn) throws Exception {
 		List<StkSearchCondition> scs = JdbcUtils.list(conn, "select * from stk_search_condition order by nvl(update_time, insert_time)", StkSearchCondition.class);
 		List<Strategy> list = new ArrayList();
@@ -138,7 +138,7 @@ public class StrategyManager {
 		}
 		return list;
 	}
-	
+
 	public static List<Strategy> getStrategyFromSreenerForm(Connection conn, ScreenerForm form) throws Exception {
 		List<Strategy> list = new ArrayList();
 		if(form.getAstrategy() != null && form.getAstrategy().length > 0){
@@ -154,58 +154,58 @@ public class StrategyManager {
 		}
 		return list;
 	}
-	
+
 	public static List<Index> getIndexFromSearchCondition(Connection conn, int id) {
 		StkSearchCondition sc = JdbcUtils.load(conn, "select * from stk_search_condition where id=?", id, StkSearchCondition.class);
 		ScreenerForm form = (ScreenerForm)JsonUtils.getObject4Json(sc.getText(), ScreenerForm.class);
 		return getIndexBySreenerForm(conn, form);
 	}
-	
+
 	public static List<Index> getIndexBySreenerForm(Connection conn, ScreenerForm form) {
 		List<String> codes = getCodeBySreenerForm(conn, form);
 		return IndexUtils.codeToIndex(conn, codes);
 	}
-	
+
 	public static List<String> getCodeBySreenerForm(Connection conn, ScreenerForm form) {
 		ScreenerAction action = new ScreenerAction();
 		String sql = action.formToSql(conn, form, "code");
 		return JdbcUtils.list(conn, sql, String.class);
 	}
-	
+
 	public static List<Index> getFollowIndex(Connection conn) throws Exception {
 		Set<String>	followStks = null;
 		try{
-			followStks = XueqiuUtils.getFollowStks("锟斤拷注C");
-			followStks.addAll(XueqiuUtils.getFollowStks("锟斤拷选"));
+			followStks = XueqiuUtils.getFollowStks("关注C");
+			followStks.addAll(XueqiuUtils.getFollowStks("我的"));
 			IOUtils.writeLines(followStks, null, new FileOutputStream(new File("d:\\care.txt")));
 		}catch(Exception e){
 			followStks = new HashSet(IOUtils.readLines(new FileInputStream("d:\\care.txt")));
-			EmailUtils.send("雪锟斤拷锟斤拷取锟斤拷锟斤拷注C锟斤拷失锟斤拷", e);//锟斤拷锟斤拷锟斤拷锟斤拷锟斤拷锟絏ueqiuUtils锟斤拷cookie锟芥换锟斤拷锟斤拷锟铰碉拷			
+			//EmailUtils.send("雪锟斤拷锟斤拷取锟斤拷锟斤拷注C锟斤拷失锟斤拷", e);//锟斤拷锟斤拷锟斤拷锟斤拷锟斤拷锟絏ueqiuUtils锟斤拷cookie锟芥换锟斤拷锟斤拷锟铰碉拷
 		}
-		
+
 		return IndexUtils.codeToIndex(conn, followStks);
 	}
-	
+
 	public static List<Index> getEarningForecastIndex(Connection conn) throws Exception {
 		List<String> codes = JdbcUtils.list(conn, "select code from stk_search_mview v where 1=1 and v.Gross_Profit_Margin >= 20.0 and v.er_low >= 30.0 and v.er_pe >= 0.0 and er_date=?",StkUtils.getNextQuarter(StkUtils.getToday()), String.class);
 		return IndexUtils.codeToIndex(conn, codes);
 	}
-	
+
 	public static List<Index> getIndexMA20And120(List<Index> indexs, String today) throws Exception {
 		List<Index> results = new ArrayList<Index>();
 		for(Index index : indexs){
-			if(index.isStop(today))continue;			
+			if(index.isStop(today))continue;
 			K k = index.getK(today);
 			if(k==null)continue;
 			K yk = k.before(1);
-			if(k.getMA(K.Close, 20) <= yk.getMA(K.Close, 20) 
+			if(k.getMA(K.Close, 20) <= yk.getMA(K.Close, 20)
 					&& (k.getMA(K.Close, 120) >= yk.getMA(K.Close, 120) || k.getMA(K.Close, 250) >= yk.getMA(K.Close, 250))){
 				results.add(index);
 			}
 		}
 		return results;
 	}
-	
+
 	public static List<Index> getIndexHighHsl(List<Index> indexs, String today) throws Exception {
 		List<Index> results = new ArrayList<Index>();
 		for(Index index : indexs){
@@ -226,12 +226,12 @@ public class StrategyManager {
 		}
 		return results;
 	}
-	
+
 	public static List<Index> getNetProfitGrwothGreaterThanZero(Connection conn) {
 		List<String> codes = JdbcUtils.list(conn, "select code from stk_search_mview where net_profit_growth_rate>=30 or revenue_growth_rate>=50", String.class);
 		return IndexUtils.codeToIndex(conn, codes);
 	}
-	
+
 	public static void main(String[] args) throws Exception{
 		Connection conn = DBUtil.getConnection();
 		getIndexFromSearchCondition(conn, 9);
--- src/com/stk123/spring/SpringTest.java
+++ src/com/stk123/spring/SpringTest.java
@@ -1,6 +1,6 @@
 package com.stk123.spring;
 
-import com.stk123.bo.StkIndustryType;
+import com.stk123.model.strategy.Strategy;
 import com.stk123.spring.jpa.entity.Admapp;
 import com.stk123.spring.jpa.entity.StkDataIndustryPeEntity;
 import com.stk123.spring.jpa.entity.StkIndustryTypeEntity;
@@ -12,22 +12,15 @@ import org.apache.commons.lang.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.support.Repositories;
 import org.springframework.stereotype.Component;
-import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.ClassUtils;
 
-import javax.annotation.Resource;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
 import javax.persistence.PersistenceContext;
-
 import java.util.Date;
-
-import static org.hibernate.jpa.AvailableSettings.PERSISTENCE_UNIT_NAME;
+import java.util.List;
 
 @Component
 public class SpringTest {
@@ -44,7 +37,7 @@ public class SpringTest {
     @PersistenceContext
     private EntityManager entityManager;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         SpringTest test = SpringUtils.getBean(SpringTest.class);
 
         if(StringUtils.equals("KaiFan",System.getProperty("user.name"))) {
@@ -62,6 +55,11 @@ public class SpringTest {
             admappRepository.updateTime(admapp);
 
             repostory();
+
+            List<Class<Strategy>> classes = SpringUtils.getAllSubClasses(Strategy.class, "com.stk123");
+            for(Class clazz : classes){
+                log.info(clazz.getName());
+            }
         }else {
             test.home();
         }
