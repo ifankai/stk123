99fd97bc0a811143599bca8ef208863f0847179a
ifankai <ifankai@aliyun.com>
2020-11-12 17:48:15 +0800

    1

---
+++ appserver/src/main/java/com/stk/model/typescript.d.ts
@@ -0,0 +1,27 @@
+// Generated using typescript-generator version 1.25.322 on 2020-11-12 17:19:44.
+
+export interface XqPost extends Serializable {
+    id: number;
+    title: string;
+    text: string;
+    createdAt: Date;
+    replyCount: number;
+    insertDate: Date;
+    isFavorite: boolean;
+    favoriteDate: Date;
+    isRead: boolean;
+    readDate: Date;
+    userId: number;
+    userName: string;
+    userAvatar: string;
+}
+
+export interface RequestResult<T> extends Serializable {
+    success: boolean;
+    msg: string;
+    code: number;
+    data: T;
+}
+
+export interface Serializable {
+}
