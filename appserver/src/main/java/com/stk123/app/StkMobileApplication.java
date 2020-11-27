2755059bb3b8797f87a1cbf133ab9d006cf2d710
ifankai <ifankai@aliyun.com>
2020-11-27 16:02:52 +0800

    1

--- appserver/src/main/java/com/stk123/app/StkMobileApplication.java
+++ appserver/src/main/java/com/stk123/app/StkMobileApplication.java
@@ -3,6 +3,7 @@ package com.stk123.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 
@@ -14,6 +15,7 @@ import org.springframework.web.socket.config.annotation.EnableWebSocket;
  */
 @SpringBootApplication
 @EnableAutoConfiguration
+@EntityScan({"com.stk123.entity"})
 @EnableAsync
 public class StkMobileApplication {
 
--- service/src/main/java/com/stk123/ServiceApplication.java
+++ service/src/main/java/com/stk123/ServiceApplication.java
@@ -1,19 +1,34 @@
 package com.stk123;
 
+import lombok.extern.apachecommons.CommonsLog;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableAsync;
+
+import javax.annotation.PostConstruct;
 
 @SpringBootApplication
 @EnableAutoConfiguration
 @EnableJpaRepositories
 @EnableAsync
+@CommonsLog
 public class ServiceApplication {
+
+    @Autowired
+    Environment environment;
 
     public static void main(String[] args) throws Exception {
         SpringApplication.run(ServiceApplication.class, args);
+    }
+
+    @PostConstruct
+    public void run() {
+        log.info(environment.getProperty("logging.file"));
     }
 
 }
