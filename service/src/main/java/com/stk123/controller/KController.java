fd3ac0909a15b03adcbea85ddb0335b9a059537a
ifankai <ifankai@aliyun.com>
2020-11-29 22:58:30 +0800

    1

---
+++ service/src/main/java/com/stk123/controller/KController.java
@@ -0,0 +1,42 @@
+package com.stk123.controller;
+
+import com.stk123.entity.StkKlineEntity;
+import com.stk123.repository.StkKlineRepository;
+import lombok.extern.apachecommons.CommonsLog;
+import org.apache.commons.lang.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.sql.Connection;
+import java.util.List;
+
+@Controller
+@RequestMapping("/k")
+@CommonsLog
+public class KController {
+
+    @Autowired
+    private StkKlineRepository stkKlineRepository;
+
+    @RequestMapping("/{code}")
+    @ResponseBody
+    public List<StkKlineEntity> getKs(@PathVariable("code")String code,
+                                      @RequestParam(value = "type", required = false, defaultValue = "1")int type,
+                                      @RequestParam(value = "days", required = false, defaultValue = "1000")int days,
+                                      @RequestParam(value = "fromDate", required = false)String fromDate,
+                                      @RequestParam(value = "toDate", required = false)String toDate) throws Exception {
+        List<StkKlineEntity> ks = null;
+        switch (type){
+            case 1:
+            case 2:
+            case 3:
+                ks = stkKlineRepository.queryTopNByCodeOrderByKlineDateDesc(code, days);
+        }
+        return ks;
+    }
+
+}
---
+++ service/src/main/java/com/stk123/repository/StkKlineRepository.java
@@ -0,0 +1,16 @@
+package com.stk123.repository;
+
+import com.stk123.entity.StkKlineEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface StkKlineRepository extends JpaRepository<StkKlineEntity, StkKlineEntity.CompositeKey> {
+
+    @Query(value = "select * from (select * from stk_kline t where code=:code order by kline_date desc) where rownum <= :rn", nativeQuery = true)
+    List<StkKlineEntity> queryTopNByCodeOrderByKlineDateDesc(@Param("code")String code, @Param("rn")Integer rn);
+}
