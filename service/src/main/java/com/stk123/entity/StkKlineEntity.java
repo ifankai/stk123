a0468fc47c01730dc6644b9c7c8e74c519202ed0
ifankai <ifankai@aliyun.com>
2020-12-03 22:53:16 +0800

    1

--- service/src/main/java/com/stk123/entity/StkKlineEntity.java
+++ service/src/main/java/com/stk123/entity/StkKlineEntity.java
@@ -1,6 +1,9 @@
 package com.stk123.entity;
 
+import com.alibaba.fastjson.annotation.JSONField;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
@@ -19,78 +22,97 @@ import java.util.Objects;
 public class StkKlineEntity implements Serializable {
     @Id
     @Column(name = "CODE", nullable = true, length = 10)
+    @JsonIgnore
     private String code;
 
     @Id
     @Column(name = "KLINE_DATE", nullable = true, length = 8)
+    @JsonProperty("d")
     private String klineDate;
 
     @Basic
     @Column(name = "OPEN", nullable = true, precision = 2)
+    @JsonProperty("o")
     private Double open;
 
     @Basic
     @Column(name = "CLOSE", nullable = true, precision = 2)
+    @JsonProperty("c")
     private Double close;
 
     @Basic
     @Column(name = "LAST_CLOSE", nullable = true, precision = 2)
+    @JsonProperty("lc")
     private Double lastClose;
 
     @Basic
     @Column(name = "HIGH", nullable = true, precision = 2)
+    @JsonProperty("h")
     private Double high;
 
     @Basic
     @Column(name = "LOW", nullable = true, precision = 2)
+    @JsonProperty("l")
     private Double low;
 
     @Basic
     @Column(name = "VOLUMN", nullable = true, precision = 2)
+    @JsonProperty("v")
     private Double volumn;
 
     @Basic
     @Column(name = "AMOUNT", nullable = true, precision = 2)
+    @JsonProperty("a")
     private Double amount;
 
     @Basic
     @Column(name = "CLOSE_CHANGE", nullable = true, precision = 6)
+    @JsonIgnore
     private Double closeChange;
 
     @Basic
     @Column(name = "HSL", nullable = true, precision = 2)
+    @JsonIgnore
     private Double hsl;
 
     @Basic
     @Column(name = "PE_TTM", nullable = true, precision = 2)
+    @JsonIgnore
     private Double peTtm;
 
     @Basic
     @Column(name = "PE_LYR", nullable = true, precision = 2)
+    @JsonIgnore
     private Double peLyr;
 
     @Basic
     @Column(name = "PERCENTAGE", nullable = true, precision = 2)
+    @JsonProperty("p")
     private Double percentage;
 
     @Basic
     @Column(name = "PS_TTM", nullable = true, precision = 2)
+    @JsonIgnore
     private Double psTtm;
 
     @Basic
     @Column(name = "PB_TTM", nullable = true, precision = 2)
+    @JsonIgnore
     private Double pbTtm;
 
     @Basic
     @Column(name = "PE_NTILE", nullable = true, precision = 0)
+    @JsonIgnore
     private Integer peNtile;
 
     @Basic
     @Column(name = "PB_NTILE", nullable = true, precision = 0)
+    @JsonIgnore
     private Integer pbNtile;
 
     @Basic
     @Column(name = "PS_NTILE", nullable = true, precision = 0)
+    @JsonIgnore
     private Integer psNtile;
 
     //private StkEntity stkByCode;
